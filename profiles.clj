decbfb4dca03238cafab129272829407a77c15b5
Christopher Mark Gore <chris.gore@rate.com>
2019-08-01 12:04:49 -0500

    much shorted profiles.clj

--- profiles.clj
+++ profiles.clj
@@ -1,7 +1,1 @@
-{:user {:plugins [[cider/cider-nrepl "0.18.0"]
-                  [jonase/eastwood "0.3.1"]
-                  [lein-ancient "0.6.15"]
-                  [lein-codox "0.9.7" :exclusions [org.clojure/clojure]]
-                  [lein-deps-tree "0.1.2"]
-                  [lein-kibit "0.1.6"]
-                  [lein-try "0.4.3"]]}}
+{:user {:plugins [[cider/cider-nrepl "0.22.0-beta3"]]}}
